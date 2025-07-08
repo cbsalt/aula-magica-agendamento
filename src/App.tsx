d92bcac9eb84e80d1fd3da4ba1fdb5ff08ca4610
Charles Sal <cbsal.tec@gmail.com>
2025-07-08 13:16:05 -0300

    feat: removed component on route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './lib/i18n';
-import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./lib/i18n";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./App.css";
 
 // Pages
-import Booking from './pages/Booking';
-import Payment from './pages/Payment';
-import TeacherDashboard from './pages/TeacherDashboard';
-import PublicBooking from './pages/PublicBooking';
-import Dashboard from './pages/Dashboard';
-import AuthCallback from './pages/AuthCallback';
+import Booking from "./pages/Booking";
+import Payment from "./pages/Payment";
+import PublicBooking from "./pages/PublicBooking";
+import Dashboard from "./pages/Dashboard";
+import AuthCallback from "./pages/AuthCallback";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -24,17 +23,18 @@ function App() {
             <Routes>
               <Route path="/" element={<Booking />} />
               <Route path="/payment" element={<Payment />} />
-              <Route path="/teacher" element={<TeacherDashboard />} />
               <Route path="/aula/:teacherId" element={<PublicBooking />} />
               <Route path="/auth/callback" element={<AuthCallback />} />
-              <Route 
-                path="/dashboard" 
+              <Route
+                path="/dashboard"
                 element={
                   <ProtectedRoute>
                     <Dashboard />
                   </ProtectedRoute>
-                } 
+                }
               />
+
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </Router>
