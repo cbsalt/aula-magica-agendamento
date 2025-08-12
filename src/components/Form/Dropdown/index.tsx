c9365cffc06ad796b1ac74b8c8aa6472795dda00
Diego Souza <dee-souza@hotmail.com>
2025-08-12 12:22:52 -0300

    dropdown with key iterator

--- src/components/Form/Dropdown/index.tsx
+++ src/components/Form/Dropdown/index.tsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import { useFormContext } from "react-hook-form";
 
 export function Dropdown({ name, label, options = [], ...props }) {
@@ -14,7 +15,7 @@ export function Dropdown({ name, label, options = [], ...props }) {
         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         {options.map(option => (
-             <option value={option.value}>{option.text}</option>
+             <option key={option.value} value={option.value}>{option.text}</option>
         ))}
       </select>
     </div>
