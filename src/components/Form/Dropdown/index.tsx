591523ddaebcb8e4e44cbaecee1a1e29ad7c3bd1
Diego Souza <dee-souza@hotmail.com>
2025-08-12 12:13:35 -0300

    refactor profile user form

---
+++ src/components/Form/Dropdown/index.tsx
@@ -0,0 +1,22 @@
+import { useFormContext } from "react-hook-form";
+
+export function Dropdown({ name, label, options = [], ...props }) {
+  const { register } = useFormContext();
+
+  return (
+    <div>
+      <label className="block text-sm font-medium text-gray-700 my-2">
+        {label}
+      </label>
+      <select
+        {...register(name)}
+        {...props}
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        {options.map(option => (
+             <option value={option.value}>{option.text}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
