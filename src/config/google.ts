8df44d26678b285b73a1024cb2b68f4751ae4bd3
Charles Sal <cbsal.tec@gmail.com>
2025-09-11 11:34:21 -0300

    feat: updated scopes

--- src/config/google.ts
+++ src/config/google.ts
@@ -3,7 +3,8 @@ export const googleConfig = {
   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
   authorization: {
     params: {
-      scope: "openid email profile https://www.googleapis.com/auth/calendar",
+      scope:
+        "openid email profile https://www.googleapis.com/auth/calendar.events",
       access_type: "offline",
       prompt: "consent",
     },
