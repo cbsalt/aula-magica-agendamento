63b639844a6cc9622f55789798abdfa57937edb5
Charles Sal <cbsal.tec@gmail.com>
2025-09-14 19:04:18 -0300

    feat: add one hour util

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,5 @@
+import { addHours, format, parse } from "date-fns";
+
 export function formatDateString(date: string) {
   return new Date(date).toLocaleTimeString("pt-BR", {
     hour: "2-digit",
@@ -6,5 +8,9 @@ export function formatDateString(date: string) {
 }
 
 export function formatPeriod(dateStart, dateEnd) {
-    return `${formatDateString(dateStart)} - ${formatDateString(dateEnd)}`
+  return `${formatDateString(dateStart)} - ${formatDateString(dateEnd)}`;
+}
+
+export function addOneHour(time: string) {
+  return format(addHours(parse(time, "HH:mm", new Date()), 1), "HH:mm");
 }
