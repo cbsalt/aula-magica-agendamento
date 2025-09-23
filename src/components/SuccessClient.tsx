18de03305f150d64efd0e53bfb5e4ba55767ce1d
Charles <cbsal.tec@gmail.com>
2025-09-23 12:10:09 -0300

    feat: refactor success message

--- src/components/SuccessClient.tsx
+++ src/components/SuccessClient.tsx
@@ -1,23 +1,9 @@
-// components/SuccessClient.tsx
 "use client";
 
-import { CalendarClock, CheckCircle2, Mail, User } from "lucide-react";
-import { useTranslation } from "react-i18next";
 import LanguageSelector from "@/components/LanguageSelector";
+import { useTranslation } from "react-i18next";
 
-interface SuccessClientProps {
-  metadata: {
-    studentName: string;
-    date: string;
-    time: string;
-  };
-}
-
-export default function SuccessClient({
-  metadata,
-}: {
-  metadata?: SuccessClientProps["metadata"];
-}) {
+export default function SuccessClient() {
   const { t } = useTranslation();
 
   return (
@@ -27,60 +13,19 @@ export default function SuccessClient({
           <LanguageSelector />
         </div>
 
-        {metadata ? (
-          <>
-            <div className="flex items-center space-x-3 text-green-600 mb-6">
-              <CheckCircle2 className="w-8 h-8" />
-              <h1 className="text-2xl font-bold">
-                {t("payment.success.payment.confirmed")}
-              </h1>
-            </div>
-            <p className="text-gray-700 mb-6">
-              {t("payment.success.payment.thanks")}{" "}
-              <span className="font-semibold">{metadata?.studentName}</span>!
+        <>
+          <h1 className="text-2xl font-bold text-green-600 mb-6">
+            {t("payment.success.payment.received")}
+          </h1>
+          <p className="text-gray-700">
+            {t("payment.success.payment.confirmation")}
+          </p>
+          <div className="mt-8 text-center">
+            <p className="text-sm text-gray-500">
+              {t("payment.success.email.info")}
             </p>
-            <div className="space-y-4 text-sm text-gray-600">
-              <div className="flex items-center space-x-2">
-                <User className="w-5 h-5 text-blue-500" />
-                <span>
-                  {t("payment.success.student")}:{" "}
-                  <strong>{metadata?.studentName}</strong>
-                </span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <CalendarClock className="w-5 h-5 text-purple-500" />
-                <span>
-                  {t("payment.success.date")}: <strong>{metadata?.date}</strong>{" "}
-                  – {t("payment.success.time")}:{" "}
-                  <strong>{metadata?.time}</strong>
-                </span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Mail className="w-5 h-5 text-red-500" />
-                <span>{t("payment.success.email.sent")}</span>
-              </div>
-            </div>
-            <div className="mt-8 text-center">
-              <p className="text-sm text-gray-500">
-                {t("payment.success.email.info")}
-              </p>
-            </div>{" "}
-          </>
-        ) : (
-          <>
-            <h1 className="text-2xl font-bold text-green-600 mb-6">
-              {t("payment.success.payment.received")}
-            </h1>
-            <p className="text-gray-700">
-              {t("payment.success.paypal.confirmation")}
-            </p>
-            <div className="mt-8 text-center">
-              <p className="text-sm text-gray-500">
-                {t("payment.success.email.info")}
-              </p>
-            </div>{" "}
-          </>
-        )}
+          </div>{" "}
+        </>
       </div>
     </main>
   );
