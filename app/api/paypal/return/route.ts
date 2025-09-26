983c4cb5d02065ce76637d676705a35ef860f43b
Charles <cbsal.tec@gmail.com>
2025-09-26 07:39:03 -0300

    feat: added retry when payment issue

--- app/api/paypal/return/route.ts
+++ app/api/paypal/return/route.ts
@@ -4,19 +4,22 @@ import { findBooking } from "@/modules/booking";
 import { PaymentService } from "@/lib/payment";
 
 export async function GET(req: NextRequest) {
+  const orderId = req.nextUrl.searchParams.get("token");
+  if (!orderId) return NextResponse.redirect("/cancel");
+
+  const paymentService = new PaymentService();
+
   try {
-    const orderId = req.nextUrl.searchParams.get("token");
-
-    if (!orderId) {
-      return NextResponse.redirect("/cancel");
-    }
-
-    const paymentService = new PaymentService();
     const captureData = await paymentService.capturePayPalPayment(orderId);
 
     const purchaseUnit = captureData.purchase_units[0];
     const capture = purchaseUnit.payments.captures[0];
     const localBookingId = capture.custom_id;
+
+    if (!localBookingId) {
+      console.error("custom_id não retornado no capture");
+      return NextResponse.redirect("/cancel");
+    }
 
     const booking = await findBooking(localBookingId);
 
@@ -27,7 +30,20 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/success`);
   } catch (err) {
-    console.error("Erro ao capturar pagamento via return_url:", err);
+    const errorData = err.response?.data;
+
+    const declined = errorData?.details?.[0]?.issue === "INSTRUMENT_DECLINED";
+    if (declined) {
+      const redirectLink = errorData?.links?.find(
+        (link) => link.rel === "redirect"
+      )?.href;
+
+      if (redirectLink) {
+        return NextResponse.redirect(redirectLink);
+      }
+    }
+
+    console.error("Falha na captura PayPal:", errorData || err.message);
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/cancel`);
   }
 }
