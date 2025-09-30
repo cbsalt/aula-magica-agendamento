eb870d120ff8adefbdce15829b2e7a66feb84751
Charles <cbsal.tec@gmail.com>
2025-09-30 17:43:27 -0300

    feat: refactor baseURL

--- src/services/teacherService.ts
+++ src/services/teacherService.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 export function createApi() {
   return axios.create({
     baseURL:
-      typeof window === "undefined"
-        ? process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
-        : "",
+      typeof window === "undefined" ? process.env.NEXT_PUBLIC_BASE_URL : "",
     headers: { "Content-Type": "application/json" },
   });
 }
