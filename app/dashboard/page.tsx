f783d74fe716df510775294476a9246eaa973cd6
Charles <cbsal.tec@gmail.com>
2025-10-06 15:51:57 -0300

    feat: refactor headers and cookies on ssr

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,19 +3,34 @@ import Footer from "@/components/Footer";
 import { authOptions } from "@/lib/auth";
 import { findTeacherByEmail, serializeTeacher } from "@/modules/teacher";
 import { getServerSession } from "next-auth";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 const WEEKS_TO_SHOW = 1;
 
-export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
-  if (!session) redirect("auth/signin");
+const customFetch = async (url: string, options: RequestInit = {}) => {
+  const hdrs = await headers();
+  const cookieStore = await cookies();
 
-  const cookieHeader = (await cookies())
+  const currentHeaders = Object.fromEntries(hdrs.entries());
+  const cookieHeader = cookieStore
     .getAll()
     .map((c) => `${c.name}=${c.value}`)
     .join("; ");
+
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...currentHeaders,
+      cookie: cookieHeader,
+      ...options.headers,
+    },
+  });
+};
+
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("auth/signin");
 
   const teacher = await findTeacherByEmail(session.user?.email, {
     paymentConfig: true,
@@ -24,15 +39,13 @@ export default async function DashboardPage() {
   const serializedTeacher = serializeTeacher(teacher);
 
   const [availabilityRes, teacherRes, previewRes] = await Promise.allSettled([
-    fetch(`${process.env.NEXTAUTH_URL}/api/teachers/me/availability`, {
-      headers: { cookie: cookieHeader },
-    }),
-    fetch(`${process.env.NEXTAUTH_URL}/api/teachers/me`, {
-      headers: { cookie: cookieHeader },
-    }),
-    fetch(`${process.env.NEXTAUTH_URL}/api/teachers/availability`, {
+    customFetch(`${process.env.NEXTAUTH_URL}/api/teachers/me/availability`),
+    customFetch(`${process.env.NEXTAUTH_URL}/api/teachers/me`),
+    customFetch(`${process.env.NEXTAUTH_URL}/api/teachers/availability`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ teacherId: teacher.id, weeks: WEEKS_TO_SHOW }),
     }),
   ]);
