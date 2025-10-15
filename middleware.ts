502e1afcb71558ed6e61c8cd435f03be422637a6
Charles <cbsal.tec@gmail.com>
2025-10-15 11:49:44 -0300

    feat: used env

--- middleware.ts
+++ middleware.ts
@@ -6,22 +6,28 @@ import { authOptions } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  const allowedOrigins = ["http://localhost:3000", "https://scheduleasier.com"];
+  const allowedOrigins = process.env.NEXTAUTH_URL;
   const origin = request.headers.get("origin");
 
-  if (origin && allowedOrigins.includes(origin)) {
+  if (origin && allowedOrigins === origin) {
     response.headers.set("Access-Control-Allow-Origin", origin);
   }
 
   response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
   response.headers.set(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
 
-  if (request.nextUrl.pathname.startsWith('/api/')) {
-    const session = await getToken({ req: request, secret: authOptions.secret });
+  if (request.nextUrl.pathname.startsWith("/api/")) {
+    const session = await getToken({
+      req: request,
+      secret: authOptions.secret,
+    });
 
     if (!session) {
       return NextResponse.json({ error: "Não autorizado" }, { status: 401 });
